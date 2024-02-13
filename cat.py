8db4f3e83fc4a7e0ca6b0cd11e316ee972bc1fba
Jay <jay.nguyen@students.fhu.edu>
2024-02-13 12:43:06 -0600

    update main.py
    first lab attempt

--- cat.py
+++ cat.py
@@ -1,6 +1,22 @@
 #Revmoe pass and complete the cat class
 class Cat():
-    pass
+    def __init__(self):
+        self.name = "Unknown"
+        self.age = 0
+    def speak(self):
+        print("Meow")
+
+import cat
+
+Stella = cat.Cat()
+Stella.name = "Stella"
+Stella.age = 7
+
+Garfield = cat.Cat()
+Garfield.name = "Garfield"
+Garfield.age = 50
+
+
 
 
 #Create objects here
